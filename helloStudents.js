594cf8a8fec0be033053f9d29da1a6248904531d
Sara Thomas <thom1461@algonquinlive.com>
2021-01-11 21:34:48 -0500

    students full names

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,25 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+function say(message) {
+    console.log(message)
+}
+
+function filterItems(arr, query) {
+    return arr.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) == 0
+    })
+}
+
+const students = require('./students.json');
+
+let lastNames = [];
+students.forEach(function(student){
+    say(`Hello ${student.firstName} ${student.lastName}`);
+    lastNames.push(student.lastName);
+});
+
+let numWithD = filterItems(lastNames, 'D');
+say("");
+say(`Number of students with last names starting with 'D': ${numWithD.length}`);
